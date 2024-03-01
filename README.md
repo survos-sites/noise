0b5edfbd72e0f2ec3a94a2dd1a9775bbd7fe2e4a
Tac Tacelosky <tacman@gmail.com>
2024-03-01 11:37:04 -0600

    add phpwa bundle, use phpwa-demo as example

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This application aims to replicate the Background Noise App using Symfony components.
 
 It is mostly a testing environment and demostration of how to deploy a Symfony-based application as a PWA, using AssetMapper and tools like https://github.com/Spomky-Labs/phpwa
+
+
+## Migration
+
+The key element of the migration is to generate the sw.js file from pwa.yaml instead of hand-crafting it. 
+
+Also, the original app did not run offline, but certainly should.
+
+The original sw.js was renamed to sw-original.js, but is only a reference.
--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    SpomkyLabs\PwaBundle\SpomkyLabsPwaBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
