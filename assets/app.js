7ece195e493df23cc2d901ee3525e079c2b3595f
Tac Tacelosky <tacman@gmail.com>
2024-03-01 12:27:39 -0600

    move assets to /assets for caching and hash

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,100 @@ import './bootstrap.js';
 import './styles/app.css'
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+
+// from index.js
+import { version } from "./js/app-info.js";
+
+const version_div = document.getElementById("version");
+version_div.innerText = `v${version}`;
+
+/**
+ * @type HTMLAudioElement
+ */
+const audio = document.getElementById("white-audio");
+
+/**
+ * @type HTMLDivElement
+ */
+const playButton = document.getElementById("play-button");
+
+/**
+ * @type HTMLDivElement
+ */
+const playInnerButton = document.getElementById("play-inner-button");
+
+/**
+ * @type Element
+ */
+const mainElement = document.getElementById("main");
+
+/**
+ * @type Element
+ */
+const menuButton = document.getElementById("menu-button");
+
+/**
+ * @type Element
+ */
+const drawer = document.getElementById("drawer");
+
+/** @type HTMLButtonElement */
+const installDialog = document.querySelector("#install-dialog");
+/** @type HTMLElement */
+const installButton = document.querySelector("#install-button");
+
+installDialog.addEventListener("ready", () => {
+    installButton.removeAttribute("hidden");
+});
+
+installButton.addEventListener("click", () => {
+    installDialog.show();
+});
+
+audio.addEventListener("timeupdate", () => {
+    // if 1 second left in audio track reset track to 0
+    // so it's continuous
+    if (audio.currentTime > audio.duration - 1) {
+        audio.currentTime = 1;
+        audio.play();
+    }
+});
+
+playButton.addEventListener("click", () => {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+
+audio.addEventListener("play", () => {
+    playInnerButton.classList.add("pause");
+    mainElement.classList.add("playing");
+});
+
+audio.addEventListener("pause", () => {
+    playInnerButton.classList.remove("pause");
+    mainElement.classList.remove("playing");
+});
+
+// side drawer setup
+menuButton.addEventListener("opened", (ev) => {
+    drawer.open = true;
+});
+drawer.addEventListener("open", (ev) => {
+    menuButton.open = true;
+});
+drawer.addEventListener("close", (ev) => {
+    menuButton.open = false;
+});
+
+document.addEventListener("keydown", (ev) => {
+    if (ev.code === "Space" && ev.target === document.body) {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    }
+});
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,16 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    #[Route('/index.html', name: 'app_index')]
     public function index(): Response
     {
         return $this->render('noise-index.html.twig', [
-            'controller_name' => 'AppController',
         ]);
     }
 }
